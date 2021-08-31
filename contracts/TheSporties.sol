eccf343718abca773ac4ecb2ee3bc112231e6da0
Gianmarco <gascenzo@empirix.com>
2021-08-31 18:19:38 +0200

    Added function for community

--- contracts/TheSporties.sol
+++ contracts/TheSporties.sol
@@ -13,32 +13,24 @@ contract TheSporties is ERC721, Ownable {
     
     event Transfer(address _to, uint _value);
 
-    uint256 public constant itemPrice = 0.06 ether;
-    uint64 public constant TOTAL_ITEMS = 10762;
-    uint64 public constant RESERVED_ITEMS = 40;
+    uint256 public constant itemPrice = 0.05 ether;
+    uint64 public constant RESERVED_ITEMS = 27;
+    uint64 public constant GIFT_ITEMS = 50;
+    uint64 public constant ADOPT_ITEMS = 7700;
+    uint64 public constant TOTAL_ITEMS = ADOPT_ITEMS + GIFT_ITEMS + RESERVED_ITEMS;
     uint128 public constant MAX_ITEMS_PER_MINT = 10;
-    string private constant IPFS_URI = "https://ipfs.io/ipfs/";
 
+    uint256 private giftItems = 0;
+    string private constant IPFS_GUESS_ITEM = "https://ipfs.io/ipfs/";
     bool public saleIsActive = false;
 
     constructor() ERC721("TheSporties", "TSPT") {
-        _setBaseURI(IPFS_URI);
-        //reserveItems(RESERVED_ITEMS);
     }
 
     function withdraw() public onlyOwner {
         uint balance = address(this).balance;
         msg.sender.transfer(balance);
         emit Transfer(msg.sender, balance);
-    }
-
-    function reserveItems(uint256 reservedItems) public onlyOwner {        
-        require(reservedItems <= 100, "No more than 100 items can be reserved");
-        uint supply = totalSupply();
-        uint i;
-        for (i = 0; i < reservedItems; i++) {
-            _safeMint(msg.sender, supply + i);
-        }
     }
     
     function flipSaleState() public onlyOwner {
@@ -53,11 +45,35 @@ contract TheSporties is ERC721, Ownable {
         _setTokenURI(tokenId, _tokenURI);
     }
 
+    function getGiftItems() public view returns( uint256 ){
+        return giftItems;
+    }
+
+
+    function reserveItems() public onlyOwner {        
+        require(totalSupply().add(RESERVED_ITEMS) <= TOTAL_ITEMS, "Reserved items would exceed max supply");
+        uint supply = totalSupply();
+        uint i;
+        for (i = 0; i < RESERVED_ITEMS; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+    }
+
+    function airdropForCommunity( address [] memory recipients ) public onlyOwner {
+        require(recipients.length + giftItems < GIFT_ITEMS + 1, "Can't gift more than 50 items");
+        require(totalSupply().add(recipients.length) <= TOTAL_ITEMS, "Gift items would exceed max supply");
+        uint256 supply = totalSupply();        
+        for( uint256 i ; i < recipients.length; i++ ){
+            _safeMint(recipients[i], supply + i );
+            giftItems += 1;
+        }
+    }
+
     function mintTheSporties(uint numberOfTokens) public payable {
-        require(saleIsActive, "Sale must be active to mint TheSporties");
-        require(numberOfTokens <= MAX_ITEMS_PER_MINT, "Can't mint more than 10 TheSporties");
-        require(totalSupply().add(numberOfTokens) <= TOTAL_ITEMS, "Purchase would exceed max supply of TheSporties");
-        require(msg.value >= itemPrice.mul(numberOfTokens), "Ether value sent is not correct");
+        require(saleIsActive, "Sale must be active to mint");
+        require(numberOfTokens <= MAX_ITEMS_PER_MINT, "Can't mint more than 10 items");
+        require(totalSupply().add(numberOfTokens) <= TOTAL_ITEMS, "Purchase would exceed max supply");
+        require(msg.value == itemPrice.mul(numberOfTokens), "Ether value sent is not correct");
         
         for(uint i = 0; i < numberOfTokens; i++) {
             uint mintIndex = totalSupply();
@@ -65,6 +81,21 @@ contract TheSporties is ERC721, Ownable {
                 _safeMint(msg.sender, mintIndex);
             }
         }
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory base = baseURI();
+        if( bytes(base).length == 0 ){
+            return IPFS_GUESS_ITEM;
+        }
+
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+
+        return string(abi.encodePacked(base, tokenId.toString()));
     }
 
 }
